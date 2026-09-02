2b9f34d7d4a872e30d96d0665281787c3770c13f
Nikhil Mathur <nikhil.a.mathur@accenture.com>
2026-09-02 23:28:34 +0530

    Fix: Remove unsupported i18n config from next.config.js for App Router

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-const { i18n } = require('./next-i18next.config.js');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n,
   images: {
     unoptimized: true,
     remotePatterns: [
