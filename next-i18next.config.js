b4f72635aa027384e9b6015b2bfd6140cd18fefd
Nikhil Mathur <nikhil.a.mathur@accenture.com>
2026-09-02 23:21:40 +0530

    Feat: Week 4 Multi-Language Support - English & हिंदी translations with language switcher

---
+++ next-i18next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'hi'],
+  },
+  ns: ['common', 'home', 'sites', 'toilets', 'paths'],
+  defaultNS: 'common',
+  localePath: './public/locales',
+  react: {
+    useSuspense: false,
+  },
+};
